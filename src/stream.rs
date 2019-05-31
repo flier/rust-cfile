7752b685da74a25170e30ae5a260b66c3e00fe31
Flier Lu <flier.lu@gmail.com>
2019-05-31 16:45:54 +0800

    fix path

--- src/stream.rs
+++ src/stream.rs
@@ -213,7 +213,7 @@ impl Stream for CFileRef {
 
     #[cfg(target_os = "linux")]
     fn file_name(&self) -> io::Result<PathBuf> {
-        use std::fs::Path;
+        use std::path::Path;
 
         let s = format!("/proc/self/fd/{}", self.as_raw_fd());
         let p = Path::new(&s);
